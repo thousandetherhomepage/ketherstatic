c518ff686eec9959dffe599a7fa639a62de285ea
Max Veytsman <maxim@ontoillogical.com>
2017-08-30 17:29:05 -0700

    Saves png and json files

--- main.go
+++ main.go
@@ -3,19 +3,28 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	_ "image/gif"
+	_ "image/jpeg"
+	"image/png"
 	"log"
 	"math/big"
+	"net/http"
+	"os"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ketherhomepage/ketherstatic/ketherhomepage"
+	"github.com/nfnt/resize"
 )
 
 const url = "http://localhost:8545"
 
 //const contractAddr = "0xffb81a3a20e7fc1d44c3222a2b7a6d5705a7064b"
-const contractAddr = "0x45d7f624d6ad9924a469d63fc20d9de5d1b16ea5"
+const contractAddr = "0xb88404dd8fe4969ef67841250baef7f04f6b1a5e"
 
 type Ad struct {
 	Idx       int    `json:"idx"`
@@ -31,7 +40,36 @@ type Ad struct {
 	ForceNSFW bool   `json:"forceNSFW"`
 }
 
+func drawAd(img *image.RGBA, ad Ad) error {
+	// First we draw the ad as a black rectangle to indicate it's bought
+	adBounds := image.Rect(ad.X, ad.Y, ad.X+ad.Width, ad.Y+ad.Height)
+
+	draw.Draw(img, adBounds, &image.Uniform{color.Black}, image.ZP, draw.Src)
+	fmt.Println(adBounds)
+	if ad.Image == "" {
+		// No ad, skip
+		return nil
+	}
+	resp, err := http.Get(ad.Image)
+	if err != nil {
+		return err
+	}
+	adImg, _, err := image.Decode(resp.Body)
+	if err != nil {
+		return err
+	}
+	scaledAdImg := resize.Resize(uint(ad.Width), uint(ad.Height), adImg, resize.Lanczos3)
+
+	draw.Draw(img, adBounds, scaledAdImg, image.ZP, draw.Src)
+	return nil
+}
+
 func main() {
+	bgColor := color.RGBA{221, 221, 221, 1}
+
+	adsImg := image.NewRGBA(image.Rect(0, 0, 1000, 1000))
+	draw.Draw(adsImg, adsImg.Bounds(), &image.Uniform{bgColor}, image.ZP, draw.Src)
+
 	conn, err := ethclient.Dial(url)
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
@@ -65,23 +103,30 @@ func main() {
 	ads := make([]Ad, length)
 
 	for i := 0; i < length; i++ {
-		ad, err := session.Ads(big.NewInt(0))
+		adData, err := session.Ads(big.NewInt(0))
 		if err != nil {
 			log.Fatalf("Failed to retrieve the ad: %v", err)
 		}
 
-		ads[i] = Ad{
+		ad := Ad{
 			Idx:       i,
-			Owner:     ad.Owner.Hex(),
-			X:         int(ad.X.Int64()),
-			Y:         int(ad.Y.Int64()),
-			Width:     int(ad.Width.Int64()),
-			Height:    int(ad.Height.Int64()),
-			Link:      ad.Link,
-			Image:     ad.Image,
-			Title:     ad.Title,
-			UserNSFW:  ad.NSFW,
-			ForceNSFW: ad.ForceNSFW,
+			Owner:     adData.Owner.Hex(),
+			X:         int(adData.X.Int64()),
+			Y:         int(adData.Y.Int64()),
+			Width:     int(adData.Width.Int64()),
+			Height:    int(adData.Height.Int64()),
+			Link:      adData.Link,
+			Image:     adData.Image,
+			Title:     adData.Title,
+			UserNSFW:  adData.NSFW,
+			ForceNSFW: adData.ForceNSFW,
+		}
+		ads[i] = ad
+
+		err = drawAd(adsImg, ad)
+		if err != nil {
+			// Don't fatal since we want to keep going
+			log.Printf("error drawing ad: %v", err)
 		}
 	}
 	json, err := json.Marshal(ads)
@@ -89,5 +134,11 @@ func main() {
 		log.Fatalf("Couldn't marshal ads to json: %v", err)
 	}
 
-	fmt.Println(string(json))
+	jsonF, _ := os.OpenFile("out.json", os.O_WRONLY|os.O_CREATE, 0600)
+	defer jsonF.Close()
+	jsonF.Write(json)
+
+	pngF, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
+	defer pngF.Close()
+	png.Encode(f, adsImg)
 }
